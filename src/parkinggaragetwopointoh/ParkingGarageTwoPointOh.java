466a4972bdc06284786538ce8cfbdd0a9d4057aa
Dan <Dan@Centurion.attlocal.net>
2015-12-21 23:43:22 -0600

    Need A or RIP the dream plz

--- src/parkinggaragetwopointoh/ParkingGarageTwoPointOh.java
+++ src/parkinggaragetwopointoh/ParkingGarageTwoPointOh.java
@@ -15,25 +15,36 @@ public class ParkingGarageTwoPointOh {
         
         GarageOwner owner1 = new GarageOwner();
         
+        //Creating a dispenser and an input for ticket production and input
         TicketDispenser td1=new TicketDispenser(new ParagraphFormatter(),owner1);
         TicketInputTerminal ti1=new TicketInputTerminal(new MinimumNoMaximumRate(),owner1);
         
+        //creating a ticket and displaying the body info
         Ticket t1=td1.printTicket();
         System.out.println(t1.getBody());
+        
+        //creating another ticket a few "hours" later
         Thread.sleep(500);
         Ticket t2=td1.printTicket();
+        //and printing the body
         System.out.println(t2.getBody());
         
+        //the car is parked for a another couple of "hours"
         Thread.sleep(5000);
+        //and the first one is submitted
         ti1.enterTicket(t1);
         
+        //sales info is logged for that transaction
         owner1.logSalesInfo();
         
+        //more waiting
         Thread.sleep(5000);
         ti1.enterTicket(t2);
         
+        //entry logged
         owner1.logSalesInfo();
         
+        //retrieval
         owner1.retrieveSalesInfo();
         
     }
